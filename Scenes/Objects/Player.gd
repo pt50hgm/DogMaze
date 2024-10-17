082175821866b4d398a7eac6864697d38236d9bd
Garv <garv1908@gmail.com>
2024-10-17 14:36:15 -0400

    add input mapping to include WASD input

--- Scenes/Objects/Player.gd
+++ Scenes/Objects/Player.gd
@@ -1,34 +1,18 @@
 extends KinematicBody2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-# func _ready():
-	# pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
 var speed = 300
 var velocity = Vector2()
 
 func get_input():
 	# Detect up/down/left/right keystate and only move when pressed
 	velocity = Vector2()
-	if Input.is_action_pressed('ui_right'):
+	if Input.is_action_pressed('right'):
 		velocity.x += 1
-	if Input.is_action_pressed('ui_left'):
+	if Input.is_action_pressed('left'):
 		velocity.x -= 1
-	if Input.is_action_pressed('ui_down'):
+	if Input.is_action_pressed('down'):
 		velocity.y += 1
-	if Input.is_action_pressed('ui_up'):
+	if Input.is_action_pressed('up'):
 		velocity.y -= 1
 	velocity = velocity.normalized() * speed
 
