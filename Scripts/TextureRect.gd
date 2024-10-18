11764818db350fe84e4cd6e0730181a90c37c238
Peter Ton <pt50hgm@gmail.com>
2024-10-18 09:59:12 -0400

    Pixel Shader
    -Added shader and viewport in "Pixelated.tscn"

---
+++ Scripts/TextureRect.gd
@@ -0,0 +1,19 @@
+extends TextureRect
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var viewport : Viewport = get_node("/root/Main/ViewportContainer/Viewport")
+	var vTexture = viewport.get_texture()
+	texture = vTexture
+	
