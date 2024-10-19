4c69c433cc5f668ec622253e57ee48279a61aae4
Peter Ton <pt50hgm@gmail.com>
2024-10-19 15:41:29 -0400

    Dog and mimic behaviour
    - Dog states and code refactored
    - Mimic now has "Chase" and "wander" states

--- Scripts/Mimic.gd
+++ Scripts/Mimic.gd
@@ -1,14 +1,57 @@
-extends KinematicBody2D
+extends "res://Scripts/Pathfinder.gd"
 
-onready var Player = get_node(Util.levelPath + "/Player")
 export var jumpscareDistance : float
+export var wanderToChaseDistance : int = 2500
+export var chaseToWanderDelay : float = 10.0
+export var wanderToWanderDelay : float = 10.0
+
+var startPos : Vector2
 
 func _ready():
-	pass # Replace with function body.
+	state = "wander"
+	startPos = position
 
+
+func set_state(s):
+	state = s
+	changeStateTimer = 0
+	if s == "wander":
+		set_target_location(player.position)
+		moveSpeed = 5000.0
+	elif s == "chase":
+		moveSpeed = 10000.0
+
+
+func stateWander(delta):
+	changeStateTimer += delta
+	if changeStateTimer > wanderToWanderDelay:
+		set_state("wander")
+	if is_illuminated() \
+	or position.distance_squared_to(player.position) < wanderToChaseDistance*wanderToChaseDistance:
+		set_state("chase")
+		
+func stateChase(delta):
+	set_target_location(player.position)
+	
+	changeStateTimer += delta
+	if changeStateTimer > chaseToWanderDelay:
+		set_state("wander")
+
+func do_state_action(delta):
+	if state == "wander":
+		stateWander(delta)
+	elif state == "chase":
+		stateChase(delta)
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if self.position.distance_squared_to(Player.position) < jumpscareDistance:
+func _physics_process(delta):
+	do_state_action(delta)
+	if Input.is_action_pressed("call_dog"):
+		set_state("call")
+		moveSpeed = 10000.0
+	
+	move(delta)
+	if self.position.distance_squared_to(player.position) < jumpscareDistance*jumpscareDistance:
 		# queue jumpscare
 		pass
