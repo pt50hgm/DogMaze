a154cc64dac4ed52bf293f73fd8ddd372a26e62d
Peter Ton <pt50hgm@gmail.com>
2024-10-19 21:43:03 -0400

    Dog and mimic behaviour pt 2
    Finished dog and mimic behaviour

--- Scripts/Dog.gd
+++ Scripts/Dog.gd
@@ -1,10 +1,9 @@
 extends "res://Scripts/Pathfinder.gd"
 
-export var followDuration: float
-export var guideDistance: int = 2500
+export var followDuration: float = 10.0
+export var guideDistance: int = 2 * 128*3
 export var followDistance: int = 100
 export var runAwayDistance: int = 3 * 128 * 3
-export var followToGuideDelay: float = 1.0
 export var guideToFollowDelay: float = 3.0
 export var scaredToFollowDelay: float = 4.0
 export var calledByMimicToFollowDelay: float = 10.0
@@ -16,48 +15,37 @@ var followPlayer = false
 var velocity : Vector2 = Vector2.ZERO
 
 var mimicToFollow : Node2D
-
+var startState = true
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	set_state("guidePlayer")
+	pass
 
 func set_state(s):
 	print(state, " ", s)
 	if s == "scared":
-		set_target_location(maze.get_random_nearby_pos(position, runAwayDistance))
+		var pos = maze.get_random_nearby_pos(position, runAwayDistance)
+		set_target_location(pos)
 		moveSpeed = 20000.0
-	if state == "called" or "calledByMimic" and s == "called" or "calledByMimic":
-		rng.randomize()
-		var randF = rng.randf_range(0, 1)
-		if randF > 0.5:
-			return
-	
+		state = s
+		return
 	state = s
 	changeStateTimer = 0
 	if s == "guidePlayer":
-		moveSpeed = 7500.0
+		print(level.exitPos)
+		set_target_location(level.exitPos)
+		moveSpeed = 15000.0
 	elif s == "followPlayer":
-		moveSpeed = 15000.0
+		moveSpeed = 0
 	elif s == "called":
-		moveSpeed = 15000.0
+		moveSpeed = 25000.0
 	elif s == "calledByMimic":
-		moveSpeed = 15000.0
+		moveSpeed = 25000.0
 
 func stateFollowPlayer(delta):
-	set_target_location(player.position)
-	
-	if not is_illuminated():
-		changeStateTimer -= delta
-	else:
-		changeStateTimer += delta
-	changeStateTimer = max(changeStateTimer, 0)
-	if changeStateTimer > followToGuideDelay:
+	if position.distance_squared_to(player.position) < guideDistance*guideDistance:
 		set_state("guidePlayer")
 
 func stateGuidePlayer(delta):
-	var exitPos = level.exitPos # Replace this with actual exit later
-	set_target_location(exitPos)
-	
 	if is_illuminated() and position.distance_squared_to(player.position) < guideDistance*guideDistance:
 		changeStateTimer -= delta
 	else:
@@ -99,6 +87,9 @@ func do_state_action(delta):
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
+	if startState:
+		set_state("guidePlayer")
+		startState = false
 	if Input.is_action_just_pressed("call_dog"):
 		set_state("called")
 	do_state_action(delta)
